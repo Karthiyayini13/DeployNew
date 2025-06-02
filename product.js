3e7cf5441456570b35ca703398ea9a4af4120eef
User <karthiyayinitg1312@gmail.com>
2025-06-02 11:43:53 +0530

    first deployNew

--- product.js
+++ product.js
@@ -956,7 +956,7 @@ app.delete('/cart/clear/:userId', async (req, res) => {
 
 
 
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
